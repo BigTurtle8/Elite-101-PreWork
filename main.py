b07c9f08ab8f171c4370172a8aa9e6b59d731767
BigTurtle8 <69373245+BigTurtle8@users.noreply.github.com>
2024-01-01 05:42:47 +0000

    Implemented User Information Collection
    Asks for user's name and age. Also validates the age input to ensure age is a positive whole number (or 0).

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print('Elite101 Chatbot')
-print('')
-print('Welcome! I am a chatbot designed to help you. What is your name? ')
+if __name__ == '__main__':
+  print('Elite101 Chatbot')
+  print('')
+  name = input('Welcome! I am a chatbot designed to help you. What is your name? ')
+  
+  while True:
+    try:
+      age = int(input('Hello there ' + name + '! How old are you? '))
+    except ValueError:
+      print('Sorry, that is not a valid age. Please enter a whole number.')
+      continue
+    else:
+      if age < 0:
+        print('Sorry, that is not a valid age. Please enter a positive number (or 0).')
+        continue
+      else:
+        break
