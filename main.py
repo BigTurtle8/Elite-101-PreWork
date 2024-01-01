727c826e373d50d9ac3c3fb031edfd85da3bf3e2
BigTurtle8 <69373245+BigTurtle8@users.noreply.github.com>
2024-01-01 08:35:12 +0000

    Implemented Adaptive Welcome Message
    Sends different messages based on age of user, and includes name of user as well.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+def getMessage(name, age):
+  if age < 15:
+    return 'Hello little one! How can I help you, ' + name + '?'
+  elif 15 <= age and age < 18:
+    return 'Hello! How can I help a young adult like yourself, ' + name + '?'
+  elif 18 <= age and age < 55:
+    return 'Hello! How can I be of assistance to you, ' + name + '?'
+  elif 55 <= age and age < 120:
+    return 'Hey, I remember when I was that young! How can I help, ' + name + '?'
+  elif age >= 120:
+    return 'Well, I hope I live to your age! How can I help, ' + name + '?'
+
 if __name__ == '__main__':
   print('Elite101 Chatbot')
   print('')
-  name = input('Welcome! I am a chatbot designed to help you. What is your name? ')
+  name = input('Hello! I am a chatbot designed to help you. What is your name? ')
   
   while True:
     try:
-      age = int(input('Hello there ' + name + '! How old are you? '))
+      age = int(input('Welcome ' + name + '! How old are you? '))
     except ValueError:
       print('Sorry, that is not a valid age. Please enter a whole number.')
       continue
@@ -15,3 +27,5 @@ if __name__ == '__main__':
         continue
       else:
         break
+
+  print(getMessage(name, age))  
